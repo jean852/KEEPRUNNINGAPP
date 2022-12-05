ba969cf7e3a10f3090cb9b470a76e79084931156
JeanBaptiste Allemandou <jeanbaptiste.allemandou@gmail.com>
2022-12-05 15:43:02 +0100

    models relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # has_many :activities
-  # has_many :challenges
+  has_many :activities
+  has_many :challenges
 end
