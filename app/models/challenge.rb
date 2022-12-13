9ea5e1a17bbd4e79aada3723272e18e8e0e6b4d1
JeanBaptiste Allemandou <jeanbaptiste.allemandou@gmail.com>
2022-12-13 16:32:06 +0100

    dashboard and detail page updated with live data

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -3,8 +3,11 @@ class Challenge < ApplicationRecord
   monetize :price_cents
   has_one :order, dependent: :destroy
 
-
   CHALLENGE_TYPE = ['KM', 'TIME']
+
+  def all_activities
+    return Activity.where('start_date >= ? AND start_date <= ? AND user_id = ?', self.start_date, self.end_date, self.user_id )
+  end
 
   def all_activities_km
     distance = 0
@@ -16,4 +19,17 @@ class Challenge < ApplicationRecord
     return distance
   end
 
+  def progress_text
+
+    progress_distance = self.all_activities_km / target_distance
+    progress_time = (Date.today - start_date).fdiv(end_date - start_date)
+
+    delta = progress_distance - progress_time
+
+    if delta >= 0
+      return "On track"
+    else
+      return "You are late"
+    end
+  end
 end
