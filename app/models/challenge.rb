05a8126abfaf043bdfd16a702ea10ab65f1c7b76
Victor de Quercize <vdequercize@hotmail.com>
2023-01-18 17:49:24 +0100

    removed validation rules for now, edited dashboard to differentiate distance challenges and sessions challenges

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -2,11 +2,11 @@ class Challenge < ApplicationRecord
   belongs_to :user
   monetize :price_cents
   has_one :order, dependent: :destroy
-  validates :name, presence: true
-  validates :activity_type, presence: true
-  validates :challenge_type, presence: true
-  validates :start_date, presence: true
-  validates :end_date, presence: true, comparison: { greater_than: :start_date }
+  #validates :name, presence: true
+  #validates :activity_type, presence: true
+  #validates :challenge_type, presence: true
+  #validates :start_date, presence: true
+  #validates :end_date, presence: true, comparison: { greater_than: :start_date }
 
   CHALLENGE_TYPE = ['KM', 'Sessions']
 
@@ -17,12 +17,17 @@ class Challenge < ApplicationRecord
 
   def all_activities_km
     distance = 0
-    activities = Activity.where('start_date >= ? AND start_date <= ? AND user_id = ?', self.start_date, self.end_date, self.user_id )
+    activities = Activity.where("start_date >= ? AND start_date <= ? AND user_id = ? AND activity_type = 'KM'", self.start_date, self.end_date, self.user_id )
     activities.each do |a|
       distance += a.distance
     end
     distance = distance / 1000.00
     return distance
+  end
+
+  def all_activities_sessions
+    activities = Activity.where("start_date >= ? AND start_date <= ? AND user_id = ? AND activity_type = 'Sessions'", self.start_date, self.end_date, self.user_id )
+    return activities.count
   end
 
   def progress_text
