13ae1f6158943c20502b2a8f9602babe038c1944
Victor de Quercize <vdequercize@hotmail.com>
2023-01-18 16:18:25 +0100

    form logic follows user choices

--- app/javascript/controllers/step_controller.js
+++ app/javascript/controllers/step_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = [ "step1", "step2", "step3", "step4" ]
+    static targets = [ "step1", "step2Cycling", "step2Running", "step3Distance", "step3Sessions", "step4" ]
   connect() {
     console.log("ERddddD");
   }
@@ -9,16 +9,32 @@ export default class extends Controller {
    to_step2() {
      console.log("next_step")
      this.step1Target.classList.add("d-none")
-     this.step2Target.classList.remove("d-none")
+     let type = document.querySelector("#challenge_activity_type")
+     if (type.value == "RUNNING") {
+     this.step2RunningTarget.classList.remove("d-none");
+    } else {
+      this.step2CyclingTarget.classList.remove("d-none");
+    }
    }
   to_step3() {
     console.log("next_step3")
-    this.step2Target.classList.add("d-none")
-    this.step3Target.classList.remove("d-none")
+    let type = document.querySelector("#challenge_activity_type")
+    if (type.value == "RUNNING") {
+      this.step2RunningTarget.classList.add("d-none");
+     } else {
+       this.step2CyclingTarget.classList.add("d-none");
+     }
+     let challengeType = document.querySelector("#challenge_challenge_type")
+     if (challengeType.value == "KM") {
+      this.step3DistanceTarget.classList.remove("d-none");
+     } else {
+       this.step3SessionsTarget.classList.remove("d-none");
+     }
     }
   to_step4() {
     console.log("next_step4")
-    this.step3Target.classList.add("d-none")
+    this.step3DistanceTarget.classList.add("d-none")
+    this.step3SessionsTarget.classList.add("d-none")
     this.step4Target.classList.remove("d-none")
     }
   }
