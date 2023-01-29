6e4379dc9ea4c75cf62127d725cec8a4c2c14d30
Aurelie Coudert <aurelie.proffit@gmail.com>
2023-01-29 22:27:05 +0100

    my account page, usersController creation with views and UserPolicy and NilClassPolicy

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,60 @@
+class UsersController < ApplicationController
+  include Pundit
+  before_action :authorize_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:edit, :update]
+  authorize_resource
+
+  def index
+    @users = policy_scope(User)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = current_user
+    authorize @user
+  end
+
+  def edit
+    @user = current_user
+    authorize @user
+  end
+
+  def update
+    @user = current_user
+    authorize @user
+    @user.update(user_params)
+    if @user.valid?
+      flash[:success] = "Your details have been updated."
+      redirect_to user_root_path
+    else
+      flash[:error] = "There was an error updating your details: #{@user.errors.full_messages.join(', ')}"
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
+
+  def authorize_user
+    authorize @user
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
