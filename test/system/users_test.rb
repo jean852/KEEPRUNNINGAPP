7b667aabb329471dcf49b0ebbccece76e2a24a94
Axel Bagenal <axel.bagenal@gmail.com>
2023-02-15 20:17:50 +0100

    create test user can visit the sign in page and assert on p selector

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -4,7 +4,18 @@ class UsersTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit root_url # "/"
 
-    save_and_open_screenshot
+    # save_and_open_screenshot
     assert_selector "h5", text: "With Keep Running, find the best way to optimize your sport activities"
   end
+
+  test "user can visit the sign in page" do
+    visit new_user_session_path
+    # save_and_open_screenshot
+    assert_selector "p", text: "Connect now"
+  end
+
+  # test "user can sign in" do
+  #   visit /users/sign_in
+  #   click on "Connect with Strava"
+  # end
 end
