2ff85553667b460ea4c691a4ed60542244f0bc3d
Axel Bagenal <axel.bagenal@gmail.com>
2023-02-15 14:19:02 +0100

    try to fix foreign key violations in fixtures, no progress

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,19 +1,13 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # fixtures :users
+  def setup
+    @user = users(:user)
+  end
 
-  # create a unit test for the model
-  # test "user attributes must not be empty" do
-  #   # create a new user object
-  #   user = User.new
-  #   # assert that the user object is not valid
-  #   assert user.invalid?
-  #   # assert that the user object has errors on the :name attribute
-  #   assert user.errors[:name].any?
-  #   # assert that the user object has errors on the :email attribute
-  #   assert user.errors[:email].any?
-  # end
+  test "valid user" do
+    assert @user.valid?
+  end
 
   # test "create user from provider data" do
   #   provider_data = Struct.new(athele_id: 123456789, provider: '', uid: '', info: Struct.new(email: '123456789@strava.com'))
@@ -21,12 +15,12 @@ class UserTest < ActiveSupport::TestCase
   #   assert_equal '123456789@strava.com', user.email
   # end
 
-  test "create user from strava" do
-    provider_data = Struct.new(:athlete).new(Struct.new(:id).new(123_456_789))
-    user = User.create_from_strava(provider_data)
-    assert_equal '123456789@strava.com', user.email
-    assert_equal 123_456_789, user.athlete_id
-  end
+  # test "create user from strava" do
+  #   provider_data = Struct.new(:athlete).new(Struct.new(:id).new(123_456_789))
+  #   user = User.create_from_strava(provider_data)
+  #   assert_equal '123456789@strava.com', user.email
+  #   assert_equal 123_456_789, user.athlete_id
+  # end
 
   # test "calculate total distance in the last 30 days" do
   #   user = users(:one) # use fixture data or create a new user as needed
