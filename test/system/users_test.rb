0089fe58478501641e607f137b0710bac4dbce34
Axel Bagenal <axel.bagenal@gmail.com>
2023-02-16 11:33:58 +0100

    created user sign in with strava test

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -8,14 +8,25 @@ class UsersTest < ApplicationSystemTestCase
     assert_selector "h5", text: "With Keep Running, find the best way to optimize your sport activities"
   end
 
-  test "user can visit the sign in page" do
+  test "visiting the sign_in" do
     visit new_user_session_path
+
     # save_and_open_screenshot
+    # change assert_selector for the button
     assert_selector "p", text: "Connect now"
   end
 
-  # test "user can sign in" do
-  #   visit /users/sign_in
-  #   click on "Connect with Strava"
+  test "user can sign in with Strava" do
+    # user visits sign_in page and clicks on the connect button
+    visit new_user_session_path
+    find('.stravalogin').click
+    # accept_alert do
+    #   click_button '.stravalogin'
+    # end
+
+  #   # user enters his Strava credentials and authorizes
+  #   # fill_in "Email", with: "111400793@strava.com"
+  #   # find('.btn-primary').click
   # end
 end
+
