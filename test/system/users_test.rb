bec75cc887868d01a91149453ba848ebf1e37977
Axel Bagenal <axel.bagenal@gmail.com>
2023-02-22 19:31:28 +0100

    update system test with user create challenge journey

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -39,4 +39,53 @@ class UsersTest < ApplicationSystemTestCase
     # save_and_open_screenshot
     assert_equal "/dashboard", page.current_path
   end
+
+  test "user can create a challenge" do
+    # user visits dashboard and clicks on the create challenge button
+
+    visit dashboard_path
+    click_link "New Challenge"
+    assert_current_path new_challenge_path
+
+    # user fills in the challenge name and clicks on which activity type they want to challenge and the continue button
+
+    find('#challenge_name').fill_in with: "My test challenge"
+    find('div[data-action="click->activity#running"]').click
+    find('div[data-action="click->step#to_step2"]').click
+
+    # user selects the challenge type (How far) and clicks on the continue button
+
+    assert_selector('.supertitle-gradient', text: "Select your")
+    find('div[data-action="click->challengetype#km"]').click
+    find('div[data-action="click->step#to_step3"]').click
+
+    # user fills in the form and clicks on the create button
+
+    find('#challenge_target_distance').fill_in with: 100
+    find('#challenge_start_date').fill_in with: "2020-03-01"
+    find('#challenge_end_date').fill_in with: "2020-03-31"
+    find('div[data-action="click->step#to_step4"]').click
+
+    # user creates an optional bet
+
+    assert_selector "h2", text: "Bet on your success!"
+    find('#challenge_price').fill_in with: 10
+    click_button 'Create Challenge'
+    assert_selector('.supertitle-gradient', text: "Time")
+    find('#pay').click
+
+    # user is redirected to stripe and fills in the payment details
+
+    find('#email').fill_in with: ENV.fetch('GMAIL_ACCOUNT')
+    find('#cardNumber').fill_in with: "4242 4242 4242 4242"
+    find('#cardExpiry').fill_in with: "01/24"
+    find('#cardCvc').fill_in with: "123"
+    find('#billingName').fill_in with: "Jean Dupont"
+    find('#billingCountry').fill_in with: "France"
+    find('.SubmitButton').click
+
+    # user is redirected to the dashboard and sees the challenge
+
+    assert_equal "/dashboard", page.current_path
+  end
 end
